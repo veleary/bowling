2d16eabd7b71b05a17081cf856226e57b3ac4f8f
MakerSquare Student <students@themakersquare.com>
2013-09-12 10:34:33 -0500

    Add expected score

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'rspec'
-require_relative '../bowling'
+require_relative '../bowling_game'
 
 describe BowlingGame do 
   describe '.initialize' do
@@ -9,6 +9,14 @@ describe BowlingGame do
       game = BowlingGame.new
 
       expect(game.rolls).to eq([])
+    end
+  end
+    describe '#score' do
+      it 'scores the game as 0 when only gutter balls are thrown' do
+        game = BowlingGame.new
+        20.times {game.roll(1)}
+
+        expect(game.score).to eq(20)
     end
   end  
 end
