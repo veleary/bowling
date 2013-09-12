12a29f23e7e0e11c0beff2acfffba7edc8d10372
veleary <veleary@gmail.com>
2013-09-12 11:51:53 -0500

    Update with Strike and Spare scoring options

--- bowling_game.rb
+++ bowling_game.rb
@@ -6,8 +6,27 @@ class BowlingGame
   end
 
   def roll(pins)
+    @rolls.push(pins)
   end
   def score
-    0
+  total_score  = 0
+  current_roll = 0
+
+  while current_roll < @rolls.size - 1
+    roll      = @rolls[current_roll]
+    next_roll = @rolls[current_roll + 1]
+
+    if roll == 10
+      total_score += 10 + @rolls[current_roll + 1] + @rolls[current_roll + 2]
+      current_roll += 1
+    elsif roll + next_roll == 10
+      total_score += 10 + @rolls[current_roll + 2]
+      current_roll += 2
+    else
+      total_score += roll + next_roll
+      current_roll += 2
+    end
   end
+  return total_score
+end
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -8,22 +8,46 @@ describe BowlingGame do
 
   describe '.initialize' do
     it 'sets @rolls to an empty array' do
-      
       expect(game.rolls).to eq([])
     end
-  end
+  end  
 
-    describe '#score' do
-      it 'scores the game as 0 when only gutter balls are thrown' do
-      20.times {game.roll(0)}
+  describe '#score' do
+    context 'when only gutter balls are thrown' do
+      it 'scores the game as 0' do
+        20.times {game.roll(0)}
 
         expect(game.score).to eq(0)
       end
-
-       it 'scores the game as 20 when only gutter balls are thrown' do
-        20.times {game.roll(1)}
-
-        expect(game.score).to eq(20)
     end
-  end  
+  end
+
+  context 'when one pin is knocked down on each roll' do
+    it 'scores the game as 20' do
+      20.times {game.roll(1)}
+
+      expect(game.score).to eq(20)
+    end
+  end
+
+  context 'when a spare is thrown' do
+    it 'records the frame score as 10 plus the number of pins knocked down in the next roll' do 
+      3.times {game.roll(5)}
+      17.times {game.roll(0)}
+
+      expect(game.score).to eq(20)
+    end
+  end
+
+  
+  context 'when a strike is thrown' do
+    it 'records the frame score as 10 plus the total number of pins knocked down in the next two rolls' do
+      game.roll(10)
+      game.roll(7)
+      game.roll(1)
+      16.times { game.roll(0) }
+
+      expect(game.score).to eq(26)
+    end
+  end
 end
